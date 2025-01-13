e564313d02cff42d18b5507fe313b46bc226b7ff
Akjol <usonov_akzol05@gmail.com>
2025-01-13 18:47:40 +0600

    finish-imageg-slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Carousel from './components/LearCraft/Carousel';
 
 
 function App() {
   
   return (
     <div className="App">
-      
+      <Carousel/>
     </div>
   );
 }
