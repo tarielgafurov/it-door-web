f288d809fc71befe5d16c85a28577c651bad87c7
Baiysh <bajysmamatisakov5@gmail.com>
2025-02-16 03:13:17 -0800

    new-main-finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import MainFinish from './layout/MainFinish';
+import OfficeGallery from './layout/OfficeGallery';
+import UsersUs from './layout/OfficeGallery';
 
 
 function App() {
   
   return (
     <div className="App">
+      <MainFinish/>
     </div>
   );
 }
